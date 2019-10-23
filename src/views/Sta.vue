be7c118eb36d0632cef624e77ec4aa4a7f123ca1
zcf <2764300331@qq.com>
2019-10-23 10:23:48 +0800

    zcf

--- src/views/Sta.vue
+++ src/views/Sta.vue
@@ -58,8 +58,10 @@ export default {
 	
  data(){
  	return {
- 		msg:"测试"
- 		
+ 		msg:"统计",
+ 		userInfo:{}, //==>js obj
+ 		startdate:"",// ==>String
+ 		enddate:"",//==>String
  	}
  },
  mounted () {
@@ -73,10 +75,57 @@ export default {
     		this.$router.push({ name: 'login', params: {}});
     	}else{
     		//用户已登录，获取用户信息
-    		//TODO
-    		console.log("用户已登录，获取用户信息");
+    		this.id = id;
+    		console.log("用户已登录，获取信息");
     	}
+    },
+    getPageInfo(){
+    	this.axios.get(API.getUserInfoById,{params:{id:this.id}})
+      .then(res => {
+        if(res.data.status==0){
+        	this.msg = res.data.err;
+        	return;
+        }
+			this.userInfo = res.data.data;
+				let currdate = new Date(); //今日日期
+				this.getWeightInfo(currdate);
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+    },
+    getWeightInfo(currdate){ // (入参:Date)
+    	let d = currdate;
+    	var enddate = d.getFullYear()+"-" + (d.getMonth()+1) + "-" + d.getDate();
+    	this.enddate = enddate;
+    	//获取6天之前的时间，注意边界值
+    	d.setTime(d.getTime()-6*24*60*60*1000);
+    	//获取6天之前的日期
+    	var startdate = d.getFullYear()+"-" + (d.getMonth()+1) + "-" + d.getDate();
+			//拼装请求参数
+			let param = {
+				startdate:startdate,
+				enddate:enddate,
+				userid:this.userInfo.id
+			};
+			//发送请求，获取一周内的体重信息
+			this.axios.get(API.getRecordbYTimeSlot,{params:param})
+      .then(res => {
+      	//是否请求成功
+        if(res.data.status==0){
+        	this.msg = res.data.err;
+        	return;
+        }
+        //重要：请求成功，全局保存用户体重信息
+			this.recordList = res.data.data;
+				//第三层：组装数据，并且渲染视图
+				this.renderUserView();
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
     }
+    
  }
  
 }
